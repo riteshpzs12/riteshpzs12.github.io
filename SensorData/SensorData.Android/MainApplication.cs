14b1c7f2f1a9efde944ecea64e4791014a3fa0c8
Ritesh Chatterjee <RC_Z0201@banfield.com>
2020-07-13 11:57:45 -0700

    Initial commit
    Skeleton Done(Orientation, Accelerometer)

---
+++ SensorData/SensorData.Android/MainApplication.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+
+namespace SensorData.Droid
+{
+    [Application]
+    public class MainApplication : Shiny.ShinyAndroidApplication<ShinySensor.SensorStartup>
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership transfer) : base(handle, transfer)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+
+            Shiny.AndroidShinyHost.Init(
+                this,
+                new ShinySensor.SensorStartup()
+            );
+        }
+    }
+}
---
+++ SensorData/SensorData/ShinySensor/SensorStartup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+using Shiny;
+
+namespace SensorData.ShinySensor
+{
+    public class SensorStartup : ShinyStartup
+    {
+        public static Dictionary<string, bool> DeviceSensorsAvailable = new Dictionary<string, bool>();
+
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            if (DeviceSensorsAvailable.ContainsKey("Accelerometer"))
+                DeviceSensorsAvailable["Accelerometer"] = services.UseAccelerometer();
+            else
+                DeviceSensorsAvailable.Add("Accelerometer", services.UseAccelerometer());
+
+            if (DeviceSensorsAvailable.ContainsKey("LightSensor"))
+                DeviceSensorsAvailable["LightSensor"] = services.UseAmbientLightSensor();
+            else
+                DeviceSensorsAvailable.Add("LightSensor", services.UseAmbientLightSensor());
+
+            if (DeviceSensorsAvailable.ContainsKey("Gyroscope"))
+                DeviceSensorsAvailable["Gyroscope"] = services.UseGyroscope();
+            else
+                DeviceSensorsAvailable.Add("Gyroscope", services.UseGyroscope());
+
+            if (DeviceSensorsAvailable.ContainsKey("Magnetometer"))
+                DeviceSensorsAvailable["Magnetometer"] = services.UseMagnetometer();
+            else
+                DeviceSensorsAvailable.Add("Magnetometer", services.UseMagnetometer());
+
+            if (DeviceSensorsAvailable.ContainsKey("Humidity"))
+                DeviceSensorsAvailable["Humidity"] = services.UseHumidity();
+            else
+                DeviceSensorsAvailable.Add("Humidity", services.UseHumidity());
+
+            if (DeviceSensorsAvailable.ContainsKey("Proximity"))
+                DeviceSensorsAvailable["Proximity"] = services.UseProximitySensor();
+            else
+                DeviceSensorsAvailable.Add("Proximity", services.UseProximitySensor());
+
+            if (DeviceSensorsAvailable.ContainsKey("HeartRate"))
+                DeviceSensorsAvailable["HeartRate"] = services.UseHeartRateMonitor();
+            else
+                DeviceSensorsAvailable.Add("HeartRate", services.UseHeartRateMonitor());
+
+            if (DeviceSensorsAvailable.ContainsKey("Barometer"))
+                DeviceSensorsAvailable["Barometer"] = services.UseBarometer();
+            else
+                DeviceSensorsAvailable.Add("Barometer", services.UseBarometer());
+
+            if (DeviceSensorsAvailable.ContainsKey("Compass"))
+                DeviceSensorsAvailable["Compass"] = services.UseCompass();
+            else
+                DeviceSensorsAvailable.Add("Compass", services.UseCompass());
+
+            if (DeviceSensorsAvailable.ContainsKey("Pedometer"))
+                DeviceSensorsAvailable["Pedometer"] = services.UsePedometer();
+            else
+                DeviceSensorsAvailable.Add("Pedometer", services.UsePedometer());
+
+            if (DeviceSensorsAvailable.ContainsKey("Temperature"))
+                DeviceSensorsAvailable["Temperature"] = services.UseTemperature();
+            else
+                DeviceSensorsAvailable.Add("Temperature", services.UseTemperature());            
+        }
+    }
+}
